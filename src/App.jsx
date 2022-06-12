dafe45efaad848efc7ed3db0f1d8f66eddfe3d54
Victor Owiti <viowiti12@gmail.com>
2022-06-12 18:56:28 +0300

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Box } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
